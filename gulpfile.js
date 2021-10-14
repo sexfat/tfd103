b563eb97753d35dae12606d0ae2f798e8a8c44f5
sexfat <bryant6831@gmail.com>
2021-10-14 14:57:04 +0800

    add aync

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,27 @@ const {
     watch
 } = require('gulp');
 
+
+// console log
 exports.testgulp =  function test(cb){
 console.log('gulp 執行成功');
  cb();
 }
+
+
+function missionA(cb) {
+    console.log('missionA');
+    cb();
+}
+
+
+function missionB(cb) {
+    console.log('missionB');
+    cb();
+}
+
+// 依序
+exports.async = series(missionA , missionB)
+
+// 同時
+exports.sync = parallel(missionA , missionB)
