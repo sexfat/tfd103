95c8eb976fab3ca9ae756b33a321c13bdd745a8a
sexfat <bryant6831@gmail.com>
2021-10-28 16:33:28 +0800

    del dist

--- a.js
+++ a.js
@@ -2,10 +2,14 @@ import $ from 'jquery';
 import { gsap } from "gsap";
 
 $('body').css('background-color' , '#eee');
+
+
 gsap.to('.box' ,{
    duration : 2,
    x: 600,
-   y : 600
+   y : 300,
+   rotation : 360,
+   repeat : 3
 })
 
 
