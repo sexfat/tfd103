33146e31e09e53b26cf73cdd7e106e8f4666e1ce
sexfat <bryant6831@gmail.com>
2021-10-28 14:37:09 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -57,18 +57,7 @@ function minicss() {
 
 exports.csscompress = minicss;
 
-// sass
-const sass = require('gulp-sass')(require('sass'));
 
-// 開發用
-function sassstyle() {
-  return src('./sass/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError)) //sass
-    .pipe(cleanCSS()) // minicss
-    .pipe(sourcemaps.write())
-    .pipe(dest('dist/css'));
-}
 //上線用sass
 function sassonline() {
   return src('./sass/*.scss')
@@ -95,17 +84,6 @@ exports.prefixer = () => (
 
 
 
-//html 樣板
-const fileinclude = require('gulp-file-include');
-
-function includeHTML() {
-  return src('*.html')
-    .pipe(fileinclude({
-      prefix: '@@',
-      basepath: '@file'
-    }))
-    .pipe(dest('./dist'));
-}
 
 // 監看
 function watchall() {
@@ -139,6 +117,55 @@ function babel5() {
 exports.js = babel5
 
 
+
+
+
+
+const clean = require('gulp-clean');
+
+function clear() {
+  return src('dist' ,{ read: false ,allowEmpty: true })
+  //不去讀檔案結構，增加刪除效率  / allowEmpty : 允許刪除空的檔案
+  .pipe(clean({force: true})); //強制刪除檔案 
+}
+
+exports.cls = clear;
+
+
+// ＝＝＝＝＝＝＝開發用 ＝＝＝＝＝＝＝
+
+
+//html 樣板
+const fileinclude = require('gulp-file-include');
+
+function includeHTML() {
+  return src('./src/*.html')
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(dest('./dist'));
+}
+
+
+// sass
+const sass = require('gulp-sass')(require('sass'));
+
+// 開發用
+function sassstyle() {
+  return src('./src/sass/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError)) //sass
+    .pipe(sourcemaps.write())
+    .pipe(dest('dist/css'));
+}
+
+// js copy
+function jsdev(){
+ return src('./src/js/*.js').pipe(dest('dist/js'));
+} 
+
+
 const browserSync = require('browser-sync');
 const reload = browserSync.reload;
 
@@ -152,24 +179,71 @@ function browser(done) {
     },
     port: 3000
   });
-  watch(['sass/*.scss', 'sass/**/*.scss'] , sassstyle).on('change' , reload);
-  watch(['*.html', 'layout/*.html'], includeHTML).on('change' , reload);
+  watch(['./src/sass/*.scss', './src/sass/**/*.scss'] , sassstyle).on('change' , reload);
+  watch(['./src/*.html', './src/layout/*.html'], includeHTML).on('change' , reload);
+  watch('./src/js/*.js', jsdev).on('change' , reload);
   done();
 }
 
+
+//開發用
 exports.default = browser;
 
 
+// ========  上線用 =========
 
-const clean = require('gulp-clean');
 
+// 開發用
+function sassonline() {
+  return src('./src/sass/*.scss')
+    .pipe(sass().on('error', sass.logError)) //sass
+    .pipe(autoprefixer({
+            cascade: false
+        }))
+    .pipe(cleanCSS()) // minicss
+    .pipe(dest('dist/css'));
+}
+
+
+//es6->es5
+function babel5() {
+    return src('./src/js/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(dest('dist/js'));
+}
+
+//  圖片壓縮
+function min_images(){
+    return src('./src/images/*.*')
+    .pipe(imagemin([
+        imagemin.mozjpeg({quality: 70, progressive: true}) // 壓縮品質      quality越低 -> 壓縮越大 -> 品質越差 
+    ]))
+    .pipe(dest('dist/images'))
+}
+
+//清除舊檔案
 function clear() {
   return src('dist' ,{ read: false ,allowEmpty: true })
   //不去讀檔案結構，增加刪除效率  / allowEmpty : 允許刪除空的檔案
   .pipe(clean({force: true})); //強制刪除檔案 
 }
 
-exports.cls = clear;
+exports.production = series(clear , parallel(includeHTML , sassonline  , babel5) ,min_images)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
