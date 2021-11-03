351ef0d0c774f9b55908cbd87d014708763ab929
sexfat <bryant6831@gmail.com>
2021-11-03 14:53:31 +0800

    add

--- vendor.js
+++ vendor.js
@@ -1,3 +1,8 @@
 // import $ from 'jquery';
-// import { gsap } from "gsap";
-import Vue from 'vue'
+import { gsap } from "gsap";
+// import Vue from 'vue'
+ gsap.to('h1' , {
+       x: 100,
+       duration: 1,
+       rotation : 180
+})
