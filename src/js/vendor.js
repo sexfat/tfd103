94b48d106ca966793d6ede02467d2304d9f5b80e
sexfat <bryant6831@gmail.com>
2021-11-03 18:52:24 +0800

    html template

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,4 +1,4 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import { gsap } from "gsap";
 // import Vue from 'vue'
  gsap.to('h1' , {
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {
     CleanWebpackPlugin
 } = require('clean-webpack-plugin');
@@ -8,7 +9,8 @@ module.exports = {
     // app = chunk name = id 
     entry: {
         index: './src/js/app.js', // index
-        app2 : './src/js/app2.js' // about
+        app2 : './src/js/app2.js' ,// about
+        vendor: './src/js/vendor.js' // 共用
     },               // 入口文件
     output: {
        path: path.resolve(__dirname, 'dist'),
@@ -36,6 +38,14 @@ module.exports = {
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
             filename: "css/[name].css" 
+        }),
+        new HtmlWebpackPlugin({
+            chunks : ['index'],  //選擇注入資源 chunk
+            inject  : 'head', //預設<body> js </body>  head or body
+            template : './src/index.html',
+            //來源
+            filename : 'index.html'
+            // 目的地
         })
     ],             // 對應的插件
     // devServer: {},           // 服務器配置
